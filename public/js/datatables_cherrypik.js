12ede8005f2fdff6559d51f76d22ab986ca61297
mehul <example@gmail.com>
2026-01-29 10:56:44 +0530

    sds

--- public/js/datatables_cherrypik.js
+++ public/js/datatables_cherrypik.js
@@ -2098,6 +2098,73 @@ function DatatableRenderFunction(
                     },
                 },
             ];
+        } else if (tableId == "#contactMessagesTable") {
+            response = [
+                {
+                    targets: 0,
+                    orderable: false,
+                    render: function (data, type, row, meta) {
+                        return `
+                    <input type="checkbox" class="multi-delete-checkbox" value="${row.id}">
+                    <span style="margin-left:8px;">
+                        ${meta.row + meta.settings._iDisplayStart + 1}
+                    </span>
+                `;
+                    },
+                },
+                {
+                    targets: 1,
+                    render: function (data, type, row, meta) {
+                        return row.name ?? '-';
+                    },
+                },
+                {
+                    targets: 2,
+                    render: function (data, type, row, meta) {
+                        return row.email ?? '-';
+                    },
+                },
+                 {
+                    targets: 3,
+                    render: function (data, type, row, meta) {
+                        return row.message ?? '-';
+                    },
+                },
+                {
+                    targets: 4,
+                    render: function (data, type, row, meta) {
+                        return row.company ?? '-';
+                    },
+                },
+                // {
+                //     targets: 3,
+                //     orderable: false,
+                //     render: function (data, type, row, meta) {
+                //         let actionBtn = "";
+
+                //         actionBtn += `
+                //     <label class="switch" title="${row.status ? 'Change Status to Inactive' : 'Change Status to Active'}">
+                //          <input type="checkbox" onclick="toggleData(this, '${row.id}', '${tableId}', '${deleteRoute}', ${numberOfActivePost})" data-id="${row.id}" ${row.status ? 'checked' : ''}>
+                //         <span class="slider"></span>
+                //     </label>
+                // `;
+
+                //         actionBtn += `
+                //     <a href="/cms/msbAppSection/${row.id}/edit" class="btn btn-oblong btn-primary btn-sm" title="Edit" style="background-color: #2d336b;">
+                //         <i class="fas fa-edit"></i>
+                //     </a>
+                // `;
+
+                //         actionBtn += `
+                //     <button class="btn btn-oblong btn-danger btn-sm" title="Delete" onclick="deleteData(this, '${tableId}', '${deleteRoute}')" data-id="${row.id}">
+                //         <i class="fa fa-trash"></i>
+                //     </button>
+                // `;
+
+                //         return actionBtn;
+                //     },
+                // },
+            ];
         }
         return response;
     }
